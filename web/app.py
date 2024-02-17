dd4004fb127be4f0d8e3b9f4e208cea52ebaf6ce
Appuchia <appuchia@appu.ltd>
2024-02-17 02:59:27 +0100

    web landing

---
+++ web/app.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("home.html")
+
+
+@app.route("/contacto/")
+def contacto():
+    return render_template("contacto.html")
