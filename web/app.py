dbc2c964018806d67d2433249eda1faf53456b0a
Appuchia <appuchia@appu.ltd>
2024-02-17 19:42:10 +0100

    Web con analíticas de los datos de usuarios

--- web/app.py
+++ web/app.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
 
-from datetime import datetime
+from datetime import date, datetime
 from flask import Flask, render_template, redirect, abort, request
-from pathlib import Path
 from werkzeug.utils import secure_filename
 
 from procesado import consumptions_csv_to_df
+from resumen import *
 
 app = Flask(__name__)
 
@@ -39,10 +39,56 @@ def resumen():
 @app.route("/resumen/<path:filename>")
 def resumen_filename(filename):
     df = consumptions_csv_to_df(f"{UPLOADS_FOLDER}/{filename}")
+
+    top5m = top_5_meses(df)
+    top5d = top_5_dias(df)
+    top5h = top_5_horas(df)
+
+    precision = 2  # Número de decimales a mostrar
+
+    consumo_plot_meses = datos_consumo_meses(df)
+    consumo_plot_dow = datos_consumo_dow(df)
+    consumo_plot_horas = datos_consumo_horas(df)
+
     return render_template(
         "resumen.html",
-        tables=[df.to_html(classes="data")],
-        data=[df.to_dict(orient="records")],
+        filename=filename,
+        top={
+            "mes": {
+                "key": top5m["Fecha_Hora"].values[0],
+                "consumo": round(top5m["Consumo_KWh"].values[0], precision),
+            },
+            "dia": {
+                "key": top5d["Fecha_Hora"].values[0],
+                "consumo": round(top5d["Consumo_KWh"].values[0], precision),
+            },
+            "hora": {
+                "key": top5h["Fecha_Hora"].values[0],
+                "consumo": round(top5h["Consumo_KWh"].values[0], precision),
+            },
+        },
+        consumos={
+            "meses": consumo_plot_meses.to_dict(orient="records"),
+            "dow": consumo_plot_dow.to_dict(orient="records"),
+            "horas": consumo_plot_horas.to_dict(orient="records"),
+        },
+    )
+
+
+# Actualización del gráfico de horas
+@app.route("/horas/<path:filename>", methods=["POST"])
+def horas(filename):
+    df = consumptions_csv_to_df(f"{UPLOADS_FOLDER}/{filename}")
+
+    fecha = date.fromisoformat(request.form["dia"])
+
+    consumo_plot_horas = datos_consumo_horas(df, fecha)
+
+    return render_template(
+        "hx-horas.html",
+        consumos=consumo_plot_horas.to_dict(orient="records"),
+        dia=fecha,
+        filename=filename,
     )
 
 
---
+++ web/resumen.py
@@ -0,0 +1,86 @@
+from datetime import date
+import pandas as pd
+
+
+def top_5_meses(df: pd.DataFrame):
+
+    # Calcular el consumo total por mes
+    consumo_por_mes = (
+        df.groupby(df["Fecha_Hora"].dt.to_period("M"))["Consumo_KWh"]
+        .sum()
+        .reset_index()
+    )
+
+    # Seleccionar los meses con mayor consumo (todos si hay menos de 3)
+    top_meses = consumo_por_mes.nlargest(min(3, len(consumo_por_mes)), "Consumo_KWh")
+
+    # print("Los meses con mayor consumo del año (ordenados de mayor a menor consumo):")
+    # print(top_meses)
+
+    return top_meses
+
+
+def top_5_dias(df: pd.DataFrame):
+
+    # Calcular el consumo total por día
+    consumo_por_dia = (
+        df.groupby(df["Fecha_Hora"].dt.date)["Consumo_KWh"].sum().reset_index()
+    )
+
+    # Seleccionar los días con mayor consumo (todos si hay menos de 10)
+    top_dias = consumo_por_dia.nlargest(min(10, len(consumo_por_dia)), "Consumo_KWh")
+
+    # print(
+    #     "\nLos días con mayor consumo del dataset (ordenados de mayor a menor consumo):"
+    # )
+    # print(top_dias)
+
+    return top_dias
+
+
+def top_5_horas(df: pd.DataFrame):
+    # Agrupar por hora y calcular la media de consumo
+    media_consumo_por_hora = (
+        df.groupby(df["Fecha_Hora"].dt.hour)["Consumo_KWh"].mean().reset_index()
+    )
+
+    # print("Media de consumo para cada hora a lo largo de todos los días:")
+    # print(media_consumo_por_hora.shape)
+
+    # Seleccionar las 5 horas con mayor consumo promedio
+    top_5_horas = media_consumo_por_hora.nlargest(5, "Consumo_KWh")
+
+    # print("\nLas 5 horas del día con mayor consumo promedio:")
+    # print(top_5_horas)
+
+    return top_5_horas
+
+
+def datos_consumo_horas(df: pd.DataFrame, dia: date = date(2023, 2, 15)):
+    # Filtrar el DataFrame para el día seleccionado
+    df_dia = df[df["Fecha_Hora"].dt.date == dia]
+
+    # Agrupar por hora y calcular la media de consumo
+    media_consumo_por_hora = (
+        df_dia.groupby(df_dia["Fecha_Hora"].dt.hour)["Consumo_KWh"].mean().reset_index()
+    )
+
+    return media_consumo_por_hora
+
+
+def datos_consumo_dow(df: pd.DataFrame):
+    # Agrupar por día de la semana y calcular la media de consumo
+    media_consumo_por_dow = (
+        df.groupby(df["Fecha_Hora"].dt.dayofweek)["Consumo_KWh"].mean().reset_index()
+    )
+
+    return media_consumo_por_dow
+
+
+def datos_consumo_meses(df: pd.DataFrame):
+    # Agrupar por mes y calcular la media de consumo
+    media_consumo_por_mes = (
+        df.groupby(df["Fecha_Hora"].dt.month)["Consumo_KWh"].mean().reset_index()
+    )
+
+    return media_consumo_por_mes
